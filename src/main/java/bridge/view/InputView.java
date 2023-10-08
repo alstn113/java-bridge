3d448cfc4154b97de806e9db5719639ad2bf0698
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-08 22:33:29 +0900

    feat: BridegeGame class refactor, test

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -33,21 +33,21 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return this.readInt("다리의 길이를 입력해주세요.");
+        return this.readInt("\n다리의 길이를 입력해주세요.");
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return this.read("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return this.read("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return this.read("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        return this.read("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 
     public <T> T retryOnException(InputFunction<T> inputFunction) {
--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,4 +1,122 @@
 package bridge.domain;
 
-public class BridgeGameTest {
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("BridgeGame 클래스 테스트")
+class BridgeGameTest {
+
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        BridgeNumberGenerator numberGenerator = new BridgeTest.TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        Bridge bridge = new Bridge(3, bridgeMaker);
+        bridgeGame = new BridgeGame(bridge);
+    }
+
+    @Test
+    @DisplayName("move 메서드 테스트")
+    void move() {
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("D");
+
+        List<String> history = bridgeGame.getHistory();
+        assertEquals("U", history.get(0));
+        assertEquals("D", history.get(1));
+        assertEquals("D", history.get(2));
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
+
+    @Nested
+    @DisplayName("isFailed 메서드 테스트")
+    class IsFailedMethodTest {
+
+
+        @Test
+        @DisplayName("isFailed 메서드 테스트 - not failed")
+        void failed() {
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            bridgeGame.move("D");
+
+            assertEquals(false, bridgeGame.isFailed());
+        }
+
+        @Test
+        @DisplayName("isFailed 메서드 테스트 - failed")
+        void notFailed() {
+            bridgeGame.move("D");
+
+            assertEquals(true, bridgeGame.isFailed());
+        }
+    }
+
+    @Nested
+    @DisplayName("getTotalAttempts 메서드 테스트")
+    class GetTotalAttemptsMethodTest {
+
+        @Test
+        @DisplayName("첫 시도로 클리어")
+        void firstAttempt() {
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            bridgeGame.move("D");
+
+            assertEquals(1, bridgeGame.getTotalAttempts());
+        }
+
+        @Test
+        @DisplayName("여러 시도로 클리어")
+        void multipleAttempts() {
+            bridgeGame.isRetry("R");
+            bridgeGame.isRetry("R");
+
+            assertEquals(3, bridgeGame.getTotalAttempts());
+        }
+    }
+
+    @Nested
+    @DisplayName("retry 메서드 테스트")
+    class RetryMethodTest {
+
+        @Test
+        @DisplayName("retry 메서드 테스트")
+        void retry() {
+            bridgeGame.move("D");
+
+            bridgeGame.retry();
+
+            assertEquals(0, bridgeGame.getHistory().size());
+            assertEquals(2, bridgeGame.getTotalAttempts());
+            assertEquals(false, bridgeGame.isFailed());
+        }
+
+    }
+
 }
