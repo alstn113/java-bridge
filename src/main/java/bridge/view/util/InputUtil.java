119ccd643ad7089c46d9c76f710ca1257d9bef3b
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-03 11:45:04 +0900

    feat:InputUtil util 작성

---
+++ src/main/java/bridge/view/util/InputUtil.java
@@ -0,0 +1,35 @@
+package bridge.view.util;
+
+import bridge.exception.ErrorMessage;
+import bridge.exception.InvalidInputException;
+import java.util.function.Supplier;
+
+public class InputUtil {
+    private InputUtil() {
+    }
+
+    public static int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(ErrorMessage.INPUT_NOT_A_NUMBER);
+        }
+    }
+
+    public static <T> T retryOnException(Supplier<T> supplier, boolean lineBreak) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                if (lineBreak) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static <T> T retryOnException(Supplier<T> supplier) {
+        return retryOnException(supplier, true);
+    }
+}
